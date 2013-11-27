1ee8d734e9abe4410c2ddc29dbcf5e40cbab0cc4
stuartm <stuart.mathews@citrix.com>
2013-11-27 12:04:01 +0000

    Made my first interaction with Toast using event driven mechanisms in android. Also included .gitignore

--- MyFirstAndroid/src/com/stuartmathews/myfirstandroid/MainActivity.java
+++ MyFirstAndroid/src/com/stuartmathews/myfirstandroid/MainActivity.java
@@ -3,12 +3,16 @@ package com.stuartmathews.myfirstandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the layout to the "activity_main" layout. See that layout to see
+        // how this activity will look like.
         setContentView(R.layout.activity_main);
     }
 
@@ -18,6 +22,18 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    public void onClick(View view )
+    {
+    	switch( view.getId())
+    	{
+    	case R.id.button1:
+    		String theMessage = getApplicationContext().getString(R.string.HelloMessage);
+    		Toast.makeText(getApplicationContext(), theMessage, Toast.LENGTH_SHORT).show();
+    		break;
+    	}
     }
     
 }
---
+++ MyFirstAndroid/src/com/stuartmathews/myfirstandroid/Utility.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.stuartmathews.myfirstandroid;
+
+/**
+ * @author Stuart Mathews
+ *
+ */
+public class Utility 
+{
+	public static int PI = 0;
+}
